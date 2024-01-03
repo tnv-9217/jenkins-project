4dda8e5a8257b107e4d4554fbd114b55168cd6fc
Tanvi Laddha <67188868+tnv-9217@users.noreply.github.com>
2024-01-03 16:56:42 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class Main{
+
+    public static void main(String[] args){
+        
+        System.out.println("This is java program");
+        System.out.println("Output from java program");
+        System.out.println(new Date());
+        System.out.println("HEllo there, github isn't behaving and not finding repo");
+        
+    }
+}
---
+++ Student.java
@@ -0,0 +1,10 @@
+public class Student{
+
+    public Student(
+        System.out.println("Creating a student object");
+    )
+
+    public void SayHello(){
+        System.out.println("hi, i m student");
+    }
+}
